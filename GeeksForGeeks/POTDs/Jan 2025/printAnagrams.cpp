72f9e6c8e7e9e47f9336fd1d04547c56686f8cca
Ashutosh Swain <79051730+AshainX@users.noreply.github.com>
2025-02-03 22:04:37 +0530

    jan 1

---
+++ GeeksForGeeks/POTDs/Jan 2025/printAnagrams.cpp
@@ -0,0 +1,92 @@
+/*Given an array of strings, return all groups of strings that are anagrams. 
+The strings in each group must be arranged in the order of their appearance 
+in the original array. Refer to the sample case for clarification.
+
+Examples:
+
+Input: arr[] = ["act", "god", "cat", "dog", "tac"]
+Output: [["act", "cat", "tac"], ["god", "dog"]]
+Explanation: There are 2 groups of anagrams "god", "dog" make group 1. "act", "cat", "tac" make group 2.
+
+Input: arr[] = ["no", "on", "is"]
+Output: [["is"], ["no", "on"]]
+Explanation: There are 2 groups of anagrams "is" makes group 1. "no", "on" make group 2.
+
+Input: arr[] = ["listen", "silent", "enlist", "abc", "cab", "bac", "rat", "tar", "art"]
+Output: [["abc", "cab", "bac"], ["listen", "silent", "enlist"], ["rat", "tar", "art"]]
+Explanation: 
+Group 1: "abc", "bac", and "cab" are anagrams.
+Group 2: "listen", "silent", and "enlist" are anagrams.
+Group 3: "rat", "tar", and "art" are anagrams.
+
+Constraints:
+1<= arr.size() <=100
+1<= arr[i].size() <=10
+
+*/
+
+
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<string>> anagrams(vector<string>& arr) {
+        // code here
+        unordered_map<string, vector<string>> groups;
+        for(string& word: arr ) {
+            string sortword = word;
+            sort(sortword.begin(), sortword.end());
+            groups[sortword].push_back(word);
+        }
+    
+        vector<vector<string>> result;
+        for (auto& pair : groups) {
+            result.push_back(pair.second);
+        }
+        
+        return result;
+    }
+    
+    
+    
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<string> arr;
+        string input_line;
+        getline(cin, input_line);
+        stringstream ss(input_line);
+        string word;
+        while (ss >> word) {
+            arr.push_back(word);
+        }
+
+        Solution ob;
+        vector<vector<string> > result = ob.anagrams(arr);
+        sort(result.begin(), result.end());
+        for (int i = 0; i < result.size(); i++) {
+            for (int j = 0; j < result[i].size(); j++) {
+                cout << result[i][j] << " ";
+            }
+            cout << "\n";
+        }
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
