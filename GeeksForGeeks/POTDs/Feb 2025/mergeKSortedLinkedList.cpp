b8f3c38620df3749daa490f32bc9035ce344ac22
Ashutosh Swain <79051730+AshainX@users.noreply.github.com>
2025-02-19 22:26:52 +0530

    feb 19

---
+++ GeeksForGeeks/POTDs/Feb 2025/mergeKSortedLinkedList.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// A Linked List node
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+/* Function to print nodes in a given linked list */
+void printList(Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    cout << endl;
+}
+
+
+// } Driver Code Ends
+/*Linked list Node structure
+
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+        #define p pair<int,Node*>
+    Node* mergeKLists(vector<Node*>& arr) {
+        // Your code here
+        if(arr.empty()) return nullptr;
+        
+        priority_queue<p, vector<p>, greater<p>> pq;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]){
+            pq.push({arr[i]->data,arr[i]});
+            }
+        }
+        Node* newHead= new Node(0);
+        Node* temp=newHead;
+        
+        //heap processing
+        while(!pq.empty()){
+            Node* curr =pq.top().second;
+            pq.pop();
+            temp->next=curr;
+            temp = temp->next;
+            if(curr->next) pq.push({curr->next->data,curr->next});
+            
+        }
+        return newHead->next;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver program to test the above functions
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n;
+        cin >> n;
+        cin.ignore();
+
+        vector<Node*> v(n);
+
+        for (int i = 0; i < n; i++) {
+            string line;
+            getline(cin, line);
+            stringstream ss(line);
+
+            Node* head = new Node(0);
+            Node* temp = head;
+            int x;
+            while (ss >> x) {
+                Node* newNode = new Node(x);
+                temp->next = newNode;
+                temp = temp->next;
+            }
+            v[i] = head->next;
+        }
+
+        Solution ob;
+        Node* head = ob.mergeKLists(v);
+        printList(head);
+    }
+
+    return 0;
+}
