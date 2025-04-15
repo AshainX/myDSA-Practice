90e050c04895b14ec2bd436444a474fa82aaf09a
Ashutosh Swain <79051730+AshainX@users.noreply.github.com>
2025-04-15 19:38:22 +0530

    invertstar

---
+++ CodeForces/CodeForces_Round_1003_Div4/C.cpp
@@ -0,0 +1,28 @@
+/*
+check if current element is greater than previous element if yes then no change and if no then check if b-ai is greater than the 
+example :abort
+
+1 4 2 5 : here 6 is given as b then we have to check that if its non decreasing so we will go like 1 4 6-2 5 which will be 1 4 4 5 this is non decreasing 
+
+*/
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+            int n,m,b,c;
+            cin>>n>>m;
+            vector<int>v(n);
+            for(int i =0;i<n;i++){
+                cin>>v[i];
+            }
+            cin>>b;
+            int flag = 0;
+            if(v[i]<=v[i+1])
+
+    }
+}
