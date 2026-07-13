d7727133280669d899c6653631381ef9ff5fe9f1
Ashutosh Swain <79051730+AshainX@users.noreply.github.com>
2026-07-13 09:27:59 +0530

    2 qns solved and progress made
    2 questions

---
+++ random_bakaiti/checkifArrayIsSorted.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isArraySorted(self, arr):
+        flag = True
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                flag = False
+        return flag
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    arr = [23,54,33,5,23,6,7,53]
+    #arr = [1,2,3,4,5,6,7,8,9]
+    print(obj.isArraySorted(arr))
